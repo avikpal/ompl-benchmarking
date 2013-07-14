72cb67ea1d7cbd6a73228dd39b8a387d8850106d
avik <avikpal.me@gmail.com>
2013-07-14 21:04:28 +0530

    simplesetup

---
+++ omplSimpleSetup.cpp
@@ -0,0 +1,36 @@
+
+
+namespace ob=ompl::base;
+namespace og=ompl::geometric;
+
+bool isStateValid(const ob::State *state){};
+
+void plainWithSimpleSetup(void){
+	ob::StateSpacePtr space(new ob:: SE3StateSpace()); //constructing the state space we are planning in
+	
+	//Setting bounds for the R3 component of this state space
+	ob::RealVectorBounds bounds(3);
+	bounds.setLow(-1);
+	bounds.setHigh(1);
+    space->as<ob::SE3StateSpace>()->setBounds(bounds);
+
+    og::SimpleSetup ss(space); //instance of ompl::geometric::SimpleSetup
+	ss.setStateValidityChecker(boost::bind(&isStateValid, _1));	
+
+	ob::ScopedState<>start(space);
+	start.random();
+
+	ob::ScopedState<>goal(space);
+	goal.random();
+	
+	ss.setStartAndGoalStates(start,goal);
+    	
+	ob::PlannerStatus solved = ss.solve(1.0);
+	
+	if(solved){
+		std::cout<<"Got the solution"<<endl;
+		ss.simplyfySolution();
+		ss.getSolutionPath().print(std::cout);	
+	}
+
+}
