092d756ecd513efc82234e6fd9205c66a35da22a
avik <avikpal.me@gmail.com>
2013-07-15 23:58:56 +0530

    validity

--- ValidityChecking.cpp
+++ ValidityChecking.cpp
@@ -2,3 +2,16 @@ bool myStateValidityCheckerFunction(const base::State *state)
 {
      return ...;
 }
+base::SpaceInformationPtr si(space);
+si->setStateValidityChecker(base::StateValidityCheckerPtr(new myStateValidityCheckerClass(si)));
+si->setStateValidityCheckingResolution(0.03); // 3%
+si->setup();
+
+class myMotionValidator : public base::MotionValidator
+{
+public:
+    // implement checkMotion()
+};
+base::SpaceInformationPtr si(space);
+si->setMotionValidator(base::MotionValidatorPtr(new myMotionValidator(si)));
+si->setup();
